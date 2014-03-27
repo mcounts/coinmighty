5c13f2116801a1c9ab3d00bce2cd435a68122808
coinmighty <coinmighty@gmail.com>
2014-03-27 17:40:49 -0400

    name change

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("SMC");
-    case mBTC: return QString("mSMC");
-    case uBTC: return QString::fromUtf8("μSMC");
+    case BTC: return QString("CMY");
+    case mBTC: return QString("mCMY");
+    case uBTC: return QString::fromUtf8("μCMY");
     default: return QString("???");
     }
 }
