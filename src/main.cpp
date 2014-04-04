66d9d4e7c4eea0ed8e9440f5beec9bd171dc5837
coinmighty <coinmighty@gmail.com>
2014-04-04 17:20:18 -0400

    live genesis block update

--- src/main.cpp
+++ src/main.cpp
@@ -2080,7 +2080,7 @@ bool LoadBlockIndex(bool fAllowNew)
 
         block.print();
         assert(false && block.GetHash() == hashGenesisBlock);
-
+        
         // Start new block file
         unsigned int nFile;
         unsigned int nBlockPos;
